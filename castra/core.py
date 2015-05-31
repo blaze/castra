fc11b54ccbeac654adb1414fc3634a56187374c9
Matthew Rocklin <mrocklin@gmail.com>
2015-05-31 21:01:48 +0200

    First commit
    Work done with @esc

---
+++ castra/core.py
@@ -0,0 +1,85 @@
+import pandas
+import tempfile
+import bloscpack
+import pickle
+from bisect import bisect
+import os
+import pandas as pd
+
+
+def escape(text):
+    return str(text)
+
+
+class Castra(object):
+    def __init__(self, columns, dtypes, index_dtype, path=None):
+        if path == None:
+            path = tempfile.mkdtemp(prefix='castra')
+            self._explicitly_given_path = False
+        else:
+            self._explicitly_given_path = True
+
+        self.path = path
+        self.columns = list(columns)
+        self.dtypes = dtypes
+        self.index_dtype = index_dtype
+
+        self.partition_list = list()
+
+    def ensure_static_meta(self, dumps=pickle.dumps):
+        for name in ['columns', 'dtypes', 'index_dtype']:
+            with open(self.dirname('meta', name), 'w') as f:
+                f.write(dumps(getattr(self, name)))
+
+    def extend(self, df):
+        # TODO: Ensure that df is consistent with existing data
+        index = df.index.values
+        partition_name = '--'.join([escape(index.min()), escape(index.max())])
+
+        os.mkdir(self.dirname(partition_name))
+
+        # Store columns
+        for col in df.columns:
+            fn = self.dirname(partition_name, col)
+            x = df[col].values
+            bloscpack.pack_ndarray_file(x, fn)
+
+        # Store index
+        fn = self.dirname(partition_name, '.index')
+        x = df.index.values
+        bloscpack.pack_ndarray_file(x, fn)
+
+        self.partition_list.append((index.max(), partition_name))
+
+    def dirname(self, *args):
+        return os.path.join(self.path, *args)
+
+    def load_partition(self, name):
+        columns = [bloscpack.unpack_ndarray_file(self.dirname(name, col))
+                   for col in self.columns]
+        index = bloscpack.unpack_ndarray_file(self.dirname(name, '.index'))
+
+        return pd.DataFrame(dict(zip(self.columns, columns)),
+                            columns=self.columns,
+                            index=pd.Index(index, dtype=self.index_dtype))
+
+    def __getitem__(self, key):
+        assert isinstance(key, slice)
+        i, j = select_partitions(self.partition_list, key)
+
+        return pd.concat([self.load_partition(name)
+                          for _, name in self.partition_list[slice(i, j)]])
+
+
+def select_partitions(partition_list, key):
+    """ Select partitions from partition list given slice
+
+    >>> pl = [(0, 'a'), (10, 'b'), (20, 'c'), (30, 'd'), (40, 'e')]
+    >>> select_partitions(pl, slice(3, 25))
+    (1, 3)
+    """
+    assert key.step is None
+    start, stop = key.start, key.stop
+    i = bisect(partition_list, (start, None)) if start is not None else None
+    j = bisect(partition_list, (stop, None)) if stop is not None else None
+    return i, j
---
+++ castra/tests/test_core.py
@@ -0,0 +1,23 @@
+from castra import Castra
+import pandas as pd
+import pandas.util.testing as tm
+
+
+A = pd.DataFrame({'x': [1, 2],
+                  'y': [1., 2.]}, columns=['x', 'y'],
+                  index=[1, 2])
+
+B = pd.DataFrame({'x': [10, 20],
+                  'y': [10., 20.]}, columns=['x', 'y'],
+                  index=[10, 20])
+
+def test_Castra():
+    c = Castra(A.columns, A.dtypes, A.index.dtype)
+    c.extend(A)
+    c.extend(B)
+
+    assert c.columns == ['x', 'y']
+
+    tm.assert_frame_equal(c[0:100], pd.concat([A, B]))
+    tm.assert_frame_equal(c[:5], A)
+    tm.assert_frame_equal(c[5:], B)
