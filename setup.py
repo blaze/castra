f21cf7ddf3700fa7cca231cecd002882737e790f
Matthew Rocklin <mrocklin@gmail.com>
2015-06-18 11:23:44 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+
+setup(name='castra',
+      version='0.1.0',
+      description='On-disk partitioned store',
+      url='http://github.com/Blosc/Castra/',
+      maintainer='Matthew Rocklin',
+      maintainer_email='mrocklin@gmail.com',
+      license='BSD',
+      keywords='',
+      packages=['castra'],
+      package_data={'castra': ['tests/*.py']},
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+      zip_safe=False)
