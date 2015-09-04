4fcc04c28e4b6a51ac0d802b2d37a493d399e7fb
Matthew Rocklin <mrocklin@gmail.com>
2015-09-04 07:17:05 -0700

    bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import exists
 from setuptools import setup
 
 setup(name='castra',
-      version='0.1.4',
+      version='0.1.5',
       description='On-disk partitioned store',
       url='http://github.com/blaze/Castra/',
       maintainer='Matthew Rocklin',
